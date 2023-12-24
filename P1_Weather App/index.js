6eb00063d47481fce087ba9b7d001075db46c840
GUNTUKU NARESH <guntuku001@gmail.com>
2023-12-24 19:27:11 +0530

    Added Js

---
+++ P1_Weather App/index.js
@@ -0,0 +1,20 @@
+const apiKey="7675127f313a1c42fd10507505d49713"
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchBox=document.querySelector(".search input")
+const searchBtn=document.querySelector(".search button")
+const weatherIcon=document.querySelector("weather-icon")
+async function checkWeather(city){
+    const response=await fetch(apiUrl +city+`&appid=${apiKey}`);
+    var data=await response.json()
+    console.log(data)
+    document.querySelector(".city").innerHTML=data.name
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°c"
+    document.querySelector(".humidity").innerHTML=data.main.humidity +"%"
+    document.querySelector(".wind").innerHTML=data.wind.speed +" km/h"
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src=   
+    }
+}
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value)
+})
